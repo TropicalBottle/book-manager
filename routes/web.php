1786ab1066e4520d0d9db44eb58fc38b2f31d7cc
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-02-13 06:54:05 +0100

    [feat](route): add post and get books and book

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', [BookController::class, 'index'])->name('books');
+Route::post('/books', [BookController::class, 'store']);
+Route::get('/book/{id}', [BookController::class, 'find']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
