56ddb2e7d2aa5509aaeeb11cd965f7237d7c9a2f
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-02-15 07:12:17 +0100

    [feat](test): Adapt page to Auth

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -5,38 +5,41 @@
         </h2>
     </x-slot>
 
-    <div class="mx-20 my-20 p-5 border border-gray-600 rounded">
-        @if(session('error'))
-            <div class="alert alert-error col-span-3">
-                <i class="inline-block sidebar-group-parent-arrow reversed-sidebar-arrow" data-eva="alert-triangle-outline" data-eva-fill="#7f1d1d"></i> {{ session('error') }}
-            </div>
-        @endif
-        @if(session('success'))
-            <div class="alert alert-success col-span-3">
-                <i class="inline-block sidebar-group-parent-arrow reversed-sidebar-arrow" data-eva="checkmark-outline" data-eva-fill="#14532D"></i>{{ session('success') }}
-            </div>
-        @endif
+    @if(Auth::user())
+        <div class="mx-20 my-20 p-5 border border-gray-600 rounded">
+            @if(session('error'))
+                <div class="alert alert-error col-span-3">
+                    <i class="inline-block sidebar-group-parent-arrow reversed-sidebar-arrow" data-eva="alert-triangle-outline" data-eva-fill="#7f1d1d"></i> {{ session('error') }}
+                </div>
+            @endif
+            @if(session('success'))
+                <div class="alert alert-success col-span-3">
+                    <i class="inline-block sidebar-group-parent-arrow reversed-sidebar-arrow" data-eva="checkmark-outline" data-eva-fill="#14532D"></i>{{ session('success') }}
+                </div>
+            @endif
 
-        <form method="post" class="mt-6 space-y-6">
-            @csrf
-            <x-input-label for="title" :value="__('Name of the book')" />
-            <x-text-input name="title" placeholder="Book name" />
+            <form method="post" class="mt-6 space-y-6">
+                @csrf
+                <x-input-label for="title" :value="__('Name of the book')" />
+                <x-text-input name="title" placeholder="Book name" />
 
-            <x-input-label for="description" :value="__('Description of the book')" />
-            <x-textarea-input name="description" placeholder="Book description" />
+                <x-input-label for="description" :value="__('Description of the book')" />
+                <x-textarea-input name="description" placeholder="Book description" />
 
-            <br>
-            <x-primary-button>{{ __('Create book') }}</x-primary-button>
-        </form>
-    </div>
-    <div class="mx-20 my-20 p-5 border border-gray-600 rounded">
+                <br>
+                <x-primary-button>{{ __('Create book') }}</x-primary-button>
+            </form>
+        </div>
+    @endif
+    <div class="grid grid-cols-2 md:grid-cols-5 gap-4 md:mx-24 md:mb-12">
         @foreach($books as $book)
-            <div class="border border-gray-600 rounded w-full my-5 p-5">
-                <h3 class="dark:text-gray-400 text-2xl">{{$book->title}}</h3>
+            <a href="{{route('book-detail', ['id' => $book->id])}}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$book->title}}</h5>
                 @if($book->description)
-                    <p class="dark:text-gray-400">{{$book->description}}</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">{{$book->description}}</p>
                 @endif
-            </div>
+            </a>
         @endforeach
     </div>
 </x-app-layout>
