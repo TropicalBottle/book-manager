12e6a4c07264376515dd48ca6efacc9aecc7c4fa
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-02-15 07:14:17 +0100

    [fix](errors): Better error management

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
@@ -16,20 +15,28 @@ class BookController extends Controller
     }
 
     public function store(Request $request) {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'title' => 'required|string',
-        ]);
+            'description' => 'nullable|string',
+        ]) ;
 
-        //@todo: meilleure validation
+        if ($validator->fails()) {
+          if (!$request->input('title')) {
+              return redirect()->back()->with('error', 'You need to add a title');
+          } else {
+              return redirect()->back()->with('error', 'An error occured');
+          }
+        }
+
         $book = new Book();
         $book->title = $request->input('title');
         $book->description = $request->input('description');
 
-        if (!$book->save()) {
-            return redirect()->back()->with('error', 'Failed to save the book');
+        if ($book->save()) {
+            return redirect()->back()->with('success', 'The book was created');
         }
 
-        return redirect()->back()->with('success', 'The book was created');
+        return redirect()->back()->with('error', 'Failed to save the book');
     }
 
     public function find($id)
@@ -39,11 +46,15 @@ class BookController extends Controller
     }
 
     public function user_list() {
-        $user = auth()->user(); //@todo: être sur que l'utilisateur est connecté
-        $books = Book::all();
-        $user_books = $user->book;
+        if (auth()->user()) {
+            $user = auth()->user(); //@todo: être sur que l'utilisateur est connecté
+            $books = Book::all();
+            $user_books = $user->book;
 
-        return view('user.my-books', ['books' => $books, 'user_books' => $user_books]);
+            return view('user.my-books', ['books' => $books, 'user_books' => $user_books]);
+        }
+
+        return redirect()->back()->with('error', 'You need to be connected to see that page');
     }
 
     public function add_book_user(Request $request) {
